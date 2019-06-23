006023fc6b4d92851a9bca16f4c4fa348987ebe3
Mohsin <mohammadmohsin@gmail.com>
2019-06-23 20:24:24 +0500

    Using jwt gem for using jwt token for authentication.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def generate_token user
+    payload = { data: {user: {id: user.id, email: user.email}} }
+    payload[:exp] = (Time.now + 10.days).to_i
+    
+    JWT.encode payload, ENV["HMAC_SECRET"], 'HS256'
+  end
 end
