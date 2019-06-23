032d8ab6b7c6537f79eed4f2df05b848ff079146
Mohsin <mohammadmohsin@gmail.com>
2019-06-23 16:56:12 +0500

    Museum api controller with sample json response

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
+
+  
+  namespace :api do
+    namespace :v1 do
+      resources :museums, only: [:index]
+    end
+  end
 end
